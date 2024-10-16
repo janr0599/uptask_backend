cbb6155b50f5447ea84f63d32529356fb96d6131
janr0599 <javiernr0599@gmail.com>
2024-10-16 19:12:17 -0400

    add status key to the task type and moved and added TaskStatus type

--- src/models/Tasks.ts
+++ src/models/Tasks.ts
@@ -1,15 +1,13 @@
 import mongoose, { Schema, Types } from "mongoose";
 import { TaskType } from "../types/Tasks";
 
-const taskStatus = {
+export const taskStatus = {
     PENDING: "pending",
     ON_HOLD: "onHold",
     IN_PROGRESS: "inProgress",
     UNDER_REVIEW: "underReview",
     COMPLETED: "completed",
 } as const;
-
-export type TaskStatus = (typeof taskStatus)[keyof typeof taskStatus];
 
 const TaskSchema: Schema = new Schema(
     {
--- src/types/Tasks.ts
+++ src/types/Tasks.ts
@@ -1,8 +1,13 @@
 import { Document, Types } from "mongoose";
+import { taskStatus } from "../models/Tasks";
 
 //Mongoose Model Type
+
+export type TaskStatus = (typeof taskStatus)[keyof typeof taskStatus];
+
 export type TaskType = Document & {
     name: string;
     description: string;
     project: Types.ObjectId;
+    status: TaskStatus;
 };

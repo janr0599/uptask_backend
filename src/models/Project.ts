2555ad364d025147f475f4f3675f9c27c89bc818
janr0599 <javiernr0599@gmail.com>
2024-11-05 11:46:44 -0400

    Add Middleware to delete project tasks and notes of those tasks when project is deleted

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,5 +1,7 @@
 import mongoose, { Schema, Types } from "mongoose";
 import { ProjectType } from "../types/Projects";
+import Task from "./Tasks";
+import Note from "./Notes";
 
 const ProjectSchema: Schema = new Schema(
     {
@@ -37,6 +39,19 @@ const ProjectSchema: Schema = new Schema(
     },
     { timestamps: true }
 );
+
+// Middleware to delete project tasks and notes of those tasks when project is deleted
+ProjectSchema.pre("deleteOne", { document: true }, async function () {
+    const projectId = this._id;
+    if (!projectId) return;
+
+    const tasks = await Task.find({ project: projectId });
+    for (const task of tasks) {
+        await Note.deleteMany({ task: task.id });
+    }
+
+    await Task.deleteMany({ project: projectId });
+});
 
 const Project = mongoose.model<ProjectType>("Project", ProjectSchema);
 export default Project;
